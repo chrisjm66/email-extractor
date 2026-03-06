c602d191a63de55dabfa22aa903b6017f0788ba6
Chris Mangan <chrisjm66@gmail.com>
2026-03-06 18:07:10 -0500

    Begin crawler work

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import requests
 from bs4 import beautifulsoup
 import re
 
+
+
 def main():
     start_url = "https://something.com"
+    SEARCH_HEADERS = {"User-Agent": "SearchCrawler/1.0"}
 
 main()
